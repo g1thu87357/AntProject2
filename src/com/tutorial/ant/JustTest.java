0871d8efd872709d27d43d433457da0dc5e73220
Aaron Lim <Aaron Lim>
2015-11-26 17:27:56 +0800

    2nd Commit

--- src/com/tutorial/ant/JustTest.java
+++ src/com/tutorial/ant/JustTest.java
@@ -4,7 +4,7 @@ public class JustTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello Ant");
+		System.out.println("Hello Ant 123");
 	}
 
 }
